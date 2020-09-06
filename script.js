e97d5ad95fe9c0a78aaf6c2c68eb4137df6ca48b
dheeraj mishra <70162627+dheeraj1465@users.noreply.github.com>
2020-09-06 15:54:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
+    ByeSpeaker(names[i]);
   }
   else {
     helloSpeaker(names[i]);
